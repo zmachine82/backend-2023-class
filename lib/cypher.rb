c9a15fe2f0aab2ac11be1b0230f56b26beb53a9e
Zack Amis <zachariah.amis@vizientinc.com>
2023-01-13 10:25:18 -0600

    refactor cypher

--- lib/cypher.rb
+++ lib/cypher.rb
@@ -1,38 +1,48 @@
 class Cypher
-    
-    def encode(msg, deviation)
-        newMsg = ""
+  def initialize
+    @char_range = (0..255).to_a
+  end
 
-        msg.each_byte do |character|
-            character = deviation + character
-            if deviation % 256 == 0
-                if deviation > 0
-                    character += 1
-                else
-                    character -= 1
-                end
-            end
-            if character > -1
-
-                while character > 255
-                    character -= 256
-                end
-            else
-                while character < 0
-                    character += 256
-                end
-            end
-            newMsg += character.chr
-        end
-        
-        return newMsg
+  def encode(msg, deviation)
+    newMsg = ""
+    msg.each_byte do |character|
+      character = @char_range[Deviation.new(deviation).value + character]
+      newMsg += character.chr
     end
 
-    def decode(msg, deviation)
-        encode(msg, -deviation)
-    end
+    return newMsg
+  end
+
+  def decode(msg, deviation)
+    encode(msg, -deviation)
+  end
 end
 
+class Deviation
+  def initialize(key)
+    @orig_key = key
+    @deviation_key = reduce_large_deviations(@orig_key)
+  end
 
+  def value
+    if @deviation_key != 0
+      return @deviation_key
+    end
+    deviation_key = @deviation_key
 
+    @orig_key > 0 ? @deviation_key + 1 : @deviation_key - 1
+  end
 
+  private
+
+  def reduce_large_deviations(deviation_key)
+    while deviation_key > 255
+      deviation_key -= 256
+    end
+
+    while deviation_key < -255
+      deviation_key += 256
+    end
+    return deviation_key
+  end
+end
